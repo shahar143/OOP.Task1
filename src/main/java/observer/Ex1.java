006c22d1ceaa78748f5389f41829675854c7a463
Ely6899 <elikozinetz@protonmail.com>
2022-12-23 13:28:12 +0200

    Added more tests to main

--- src/main/java/observer/Ex1.java
+++ src/main/java/observer/Ex1.java
@@ -15,6 +15,21 @@ public class Ex1 {
 
         groupAdmin.delete(0, 6);
 
+        System.out.println();
+
+        GroupAdmin groupAdmin2 = new GroupAdmin();
+        Member member3 = new ConcreteMember("A");
+        Member member4 = new ConcreteMember("B");
+
+        groupAdmin2.register(member3);
+        groupAdmin2.register(member4);
+
+        groupAdmin2.append("Hello");
+
+        groupAdmin.notify_members();
+
+
+
 
 
 
