9e52ecf557fb349f3602c9ebeb4016823d9e7c96
Ely6899 <elikozinetz@protonmail.com>
2022-12-23 10:42:38 +0200

    simplified code

--- src/main/java/observer/ConcreteMember.java
+++ src/main/java/observer/ConcreteMember.java
@@ -1,57 +1,18 @@
 package observer;
 
-import java.util.Stack;
-
 public class ConcreteMember implements Member{
-    private Stack<UndoableStringBuilder> updateStack;
-    private UndoableStringBuilder usb;
-    private GroupAdmin groupAdmin;
-    private final String nickName;
-    private boolean pushUpdates;
+    private final String name;
+    private UndoableStringBuilder localUSB;
 
-    public ConcreteMember(String nickName, GroupAdmin groupAdmin){
-        this.nickName = nickName;
-        setGroupAdmin(groupAdmin);
-        registerMember();
-        this.usb = groupAdmin.getCurrentString(); //Shallow copy of UndoableStringBuilder instance in the given groupAdmin.
-        this.updateStack = new Stack<>();
-        this.pushUpdates = true;
+    public ConcreteMember(String name){
+        this.name = name;
+        this.localUSB = null;
     }
+
     @Override
     public void update(UndoableStringBuilder usb) {
-        this.updateStack.push(usb);
-        if(pushUpdates){
-            // this.usb = this.updateStack.pop();
-            System.out.println("User " + this.nickName + " got notified with the message: " + this.usb);
-        }
-
-    }
-
-    public void registerMember(){
-        groupAdmin.register(this);
-    }
-
-    public Stack<UndoableStringBuilder> getUpdateStack(){
-        System.out.print(this.nickName + " update stack -> ");
-        return this.updateStack;
-    }
-
-    public boolean getPushUpdates(){
-        return this.pushUpdates;
-    }
-
-    public void setPushUpdates() {
-        this.pushUpdates = !getPushUpdates();
-        System.out.println(this.nickName + " changed push updates status to " + this.pushUpdates);
-    }
-
-    public void setGroupAdmin(GroupAdmin groupAdmin){
-        this.groupAdmin = groupAdmin;
-    }
-
-    @Override
-    public String toString() {
-        return this.nickName;
+        this.localUSB = usb;
+        System.out.println(this.name + " has a new value -> " + this.localUSB);
     }
 }
 
--- src/main/java/observer/GroupAdmin.java
+++ src/main/java/observer/GroupAdmin.java
@@ -1,65 +1,55 @@
 package observer;
 
+
 import java.util.ArrayList;
 
 public class GroupAdmin implements Sender{
-    private ArrayList<ConcreteMember> members;
-    private UndoableStringBuilder usb;
+    private ArrayList<Member> members;
+    private final UndoableStringBuilder usb;
 
     public GroupAdmin(){
-        members = new ArrayList<>();
+        this.members = new ArrayList<>();
         usb = new UndoableStringBuilder();
     }
 
     @Override
     public void register(Member obj) {
-        if(!(members.contains((ConcreteMember) obj))){
-            members.add((ConcreteMember) obj);
-            ((ConcreteMember) obj).setGroupAdmin(this);
-        }
+        members.add(obj);
     }
 
     @Override
     public void unregister(Member obj) {
-        members.remove((ConcreteMember) obj);
-        ((ConcreteMember) obj).setGroupAdmin(null);
+        members.remove(obj);
     }
 
     @Override
     public void insert(int offset, String obj) {
         usb.insert(offset, obj);
-        notify_members(usb);
+        notify_members();
     }
 
     @Override
     public void append(String obj) {
         usb.append(obj);
-        notify_members(usb);
+        notify_members();
     }
 
     @Override
     public void delete(int start, int end) {
         usb.delete(start, end);
-        notify_members(usb);
+        notify_members();
     }
 
     @Override
     public void undo() {
         usb.undo();
-        notify_members(usb);
+        notify_members();
     }
 
-    public void notify_members(UndoableStringBuilder change){
-        for (Member member : members) {
-            member.update(change);
+    public void notify_members(){
+        for(Member member: members){
+            member.update(usb);
         }
-    }
-
-    public ArrayList<ConcreteMember> getMemberList(){
-        return this.members;
-    }
-
-    public UndoableStringBuilder getCurrentString(){
-        return usb;
+        System.out.println();
     }
 }
